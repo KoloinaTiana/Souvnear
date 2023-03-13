d2bdfe6a867eca66383da3f6f0add505f95481c7
KoloinaTiana <koloina.rasolofoson@etu.univ-tours.fr>
2023-03-13 19:04:46 +0100

    Modification du nomdu projet, nouveau logo, page aide: contact par téléphone et par email.

--- app/src/main/java/com/example/projet/ui/help/HelpFragment.java
+++ app/src/main/java/com/example/projet/ui/help/HelpFragment.java
@@ -1,26 +1,34 @@
 package com.example.projet.ui.help;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.projet.R;
 import com.example.projet.databinding.FragmentHelpBinding;
 
 public class HelpFragment extends Fragment {
 
     private FragmentHelpBinding binding;
+    private static final int REQUEST_CALL = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +40,39 @@ public class HelpFragment extends Fragment {
 
         final TextView textView = binding.textHelp;
         helpViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button e_b = root.findViewById(R.id.email_button);
+        Button p_b = root.findViewById(R.id.phone_button);
+        String p = "0123456789";
+        e_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"aide@souvnear.com"});
+
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(Intent.createChooser(intent, "Envoyer l'e-mail"));
+                }
+            }
+        });
+
+        p_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = "tel:" + p;
+                Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse(phoneNumber));
+                if (ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(requireActivity(),
+                            new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                } else {
+                    startActivity(dial);
+                }
+            }
+        });
+
+
         return root;
     }
 
